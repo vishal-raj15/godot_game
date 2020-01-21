9a4fcf2041116b369bcf2962bbe8ec9a3fb01f19
vishalraj@github <50707709+vishal-raj15@users.noreply.github.com>
2020-01-21 20:51:19 +0530

    Update KinematicBody2D.gd

--- godot_pr/KinematicBody2D.gd
+++ godot_pr/KinematicBody2D.gd
@@ -1,35 +1,35 @@
-extends KinematicBody2D
-const speed = 150
-const gravity = 20
-const jump_limit = 500
-
 const UP = Vector2(0,-1)   #now right and up can be pressed at same time acts like a vector triangle
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += gravity
 	if Input.is_action_pressed("ui_right"):
-		motion.x = speed
+		motion.x = min(motion.x + acc , max_speed) 
 		$Sprite.flip_h = true
 		$Sprite.play("run")
 		
 		
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -speed
+		motion.x = max(motion.x - acc , -max_speed)
 		$Sprite.flip_h = false
 		$Sprite.play("run")
 		
 	else:
-		motion.x=0
+		
 		$Sprite.play("idle")
+		motion.x= lerp(motion.x , 0 , 0.2)  # adds friction after releasing running button
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = -jump_limit
 			
-		
 	else:
-		$Sprite.play("jump")
+		if motion.y <0:
+			$Sprite.play("jump")			# here jump and falling animation works at only single animated sprite
+		
+		else:
+			$Sprite.play("fall")            # here jump and falling animation works at only single animated sprite
+			motion.x= lerp(motion.x , 0 , 0.05)
 		
 	motion = move_and_slide(motion,UP)
 	pass
