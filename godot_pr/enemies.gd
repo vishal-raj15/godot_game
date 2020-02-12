2fd3ed581fabe41ea408c99fc9ca201383857892
vishal-raj15 <vishalraj1380@gmail.com>
2020-02-13 02:44:13 +0530

    enemies modified

---
+++ godot_pr/enemies.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+const g = 10
+const speed = 150
+const Floor = Vector2(0,-1)
+
+var velocity = Vector2()
+var direction = 1
+
+func _ready():
+	pass
+	
+
+func _physics_process(delta):
+	velocity.x = speed * direction
+	
+	if direction == 1:
+		$AnimatedSprite.flip_h = false
+	
+	else:
+		$AnimatedSprite.flip_h = true
+		
+	$AnimatedSprite.play("enemies")
+	velocity.y += g
+	velocity = move_and_slide(velocity , Floor)
+	
+	
+	if is_on_wall():
+		direction = direction * -1
+		$RayCast2D.position.x *= -1
+		
+	if $RayCast2D.is_colliding() == false:
+		direction = direction*-1
+		$RayCast2D.position.x *= -1
+		
+		
+		
+		
